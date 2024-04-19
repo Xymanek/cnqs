5b08bdfe7569996cf7df21c7e9919aabcde3e368
Xymanek <xymanek@outlook.com>
2024-04-19 09:33:30 +0300

    Disable eslint's `linebreak-style` complaining

--- spa/.eslintrc.cjs
+++ spa/.eslintrc.cjs
@@ -6,5 +6,8 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'import/extensions': 'off',
+
+    // https://stackoverflow.com/a/65519055/2588539
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
   },
 };
