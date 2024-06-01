d440a6c1f0175a9fc3defa8ecb675df5c0ed7eac
Xymanek <xymanek@outlook.com>
2024-06-01 16:36:49 +0300

    Basic file is uploaded

--- spa/src/data/backendApi.ts
+++ spa/src/data/backendApi.ts
@@ -1,14 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import {
-  CreateFileRequest,
-  CreateFileRequestToJSON,
-  CreateFileResponse,
-  CreateFileResponseFromJSON,
-  ListFilesResponse,
-  type UploadFileResponse,
-} from '@/backend-api/models';
+import axios from 'axios';
+import { ListFilesResponse, type UploadFileResponse } from '@/backend-api/models';
 import { UploadFileEndpointRequest } from '@/backend-api/apis';
+import type { components } from './backendApi.types';
+import { selectFileByClientId, setUploadProgress } from '@/data/uploader/uploaderSlice';
 import { RootState } from '@/data/store';
+
+export type BackendApiSchemas = components['schemas'];
 
 export const backendApi = createApi({
   reducerPath: 'backendApi',
@@ -31,29 +29,38 @@ export const backendApi = createApi({
         };
       },
     }),
-    createFile: builder.mutation<CreateFileResponse, CreateFileRequest>({
+    createFile: builder.mutation<
+      BackendApiSchemas['CreateFileResponse'],
+      BackendApiSchemas['CreateFileRequest']
+    >({
       query: (req) => ({
         url: '/api/files',
         method: 'POST',
-        body: CreateFileRequestToJSON(req),
+        body: req,
       }),
-      transformResponse: (baseQueryReturnValue): CreateFileResponse =>
-        CreateFileResponseFromJSON(baseQueryReturnValue),
     }),
-    transferFile: builder.mutation<CreateFileResponse, CreateFileRequest>({
+    uploadFileContent: builder.mutation<void, string>({
       queryFn: async (arg, api) => {
-        (api.getState() as RootState).uploader.files[0].file;
+        const fileToUpload = selectFileByClientId(api.getState() as RootState, arg);
+        if (!fileToUpload) throw new Error('Failed to find file to upload');
+        if (!fileToUpload.uploadUrl) throw new Error('Missing upload URL');
 
-        await new Promise(() => {});
+        await axios.put(fileToUpload.uploadUrl!, fileToUpload.file, {
+          headers: {
+            'Content-Type': fileToUpload.file.type,
+          },
+          onUploadProgress: (progressEvent) => {
+            api.dispatch(setUploadProgress({ id: arg, progress: progressEvent.progress }));
+          },
+          // cancelToken: new Ca // TODO
+        });
 
         return {
-          data: {} as CreateFileResponse,
+          data: undefined,
         };
       },
     }),
   }),
 });
-
-// backendApi.endpoints.createFile.select('').
 
 export const { useListUploadedFilesQuery, useUploadFileMutation } = backendApi;
--- spa/src/data/uploader/uploaderTrigger.ts
+++ spa/src/data/uploader/uploaderTrigger.ts
@@ -1,14 +1,13 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit';
 import { backendApi } from '../backendApi';
 import { FileToUpload, uploaderSlice } from './uploaderSlice';
-import { fileTransferApi } from '@/data/uploader/fileTransferApi';
 
 export function getCreateMutationKey(file: FileToUpload): string {
   return `upload-${file.clientId}`;
 }
 
-export function getUploadMutationKey(clientId: string): string {
-  return `upload-${clientId}`;
+export function getUploadContentMutationKey(clientId: string): string {
+  return `upload-content-${clientId}`;
 }
 
 export const uploaderTriggerMiddleware = createListenerMiddleware();
@@ -18,10 +17,6 @@ uploaderTriggerMiddleware.startListening({
   effect: (action, api) => {
     api.dispatch(
       backendApi.endpoints.createFile.initiate(
-        // {
-        //   file: new Blob([action.payload.file]),
-        //   autoShare: true,
-        // },
         {
           clientId: action.payload.clientId,
           displayName: action.payload.displayName,
@@ -37,15 +32,12 @@ uploaderTriggerMiddleware.startListening({
 uploaderTriggerMiddleware.startListening({
   matcher: backendApi.endpoints.createFile.matchFulfilled,
   effect: (action, api) => {
-    const clientId = action.meta.arg.originalArgs.clientId!;// TODO !
+    const clientId = action.meta.arg.originalArgs.clientId!; // TODO !
 
     api.dispatch(
-      fileTransferApi.endpoints.uploadFile.initiate(
-        clientId,
-        {
-          fixedCacheKey: getUploadMutationKey(clientId),
-        }
-      )
+      backendApi.endpoints.uploadFileContent.initiate(clientId, {
+        fixedCacheKey: getUploadContentMutationKey(clientId),
+      })
     );
   },
 });
